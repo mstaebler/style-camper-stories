b5b21af8d3341542bcc1b2566aaa4d9f3ca014eb
Mark Staebler <markstaebler@gmail.com>
2015-10-25 22:13:42 +0000

    first commit

---
+++ client/js/main.js
@@ -0,0 +1,12 @@
+var app = angular.module("Stories", []);
+
+app.controller("PostsCtrl", function($scope, $http) {
+  // GET JSON data
+  $http.get('js/data.json').
+    success(function(data, status, headers, config) {
+      $scope.posts = data; //save json data to our posts object
+    }).
+    error(function(data, status, headers, config) {
+      // log error
+    });
+});
